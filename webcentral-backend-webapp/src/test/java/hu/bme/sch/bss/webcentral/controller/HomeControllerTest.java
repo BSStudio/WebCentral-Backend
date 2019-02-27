c2d4c1b71744c8596a3c60b3534b09c177372f3d
Péter Veress <v-pveress@hotels.com>
2019-02-27 16:17:38 +0100

    #28: home controller unit test

--- webcentral-backend-webapp/src/test/java/hu/bme/sch/bss/webcentral/controller/HomeControllerTest.java
+++ webcentral-backend-webapp/src/test/java/hu/bme/sch/bss/webcentral/controller/HomeControllerTest.java
@@ -1,5 +1,25 @@
 package hu.bme.sch.bss.webcentral.controller;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.fail;
+
+import org.junit.jupiter.api.Test;
+
 public class HomeControllerTest {
+
+    private HomeController underTest;
+
+    @Test
+    public void testHome() {
+        // GIVEN
+        underTest = new HomeController();
+
+        // WHEN
+        String result = underTest.home();
+
+        // THEN
+        assertEquals("Hello", result);
+
+    }
 
 }
