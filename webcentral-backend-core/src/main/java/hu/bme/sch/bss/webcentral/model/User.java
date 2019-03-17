ead0646057c9a82c668d20b2e4b2656fb53e6a1f
csikbence@edu.bme.hu <csikbence@edu.bme.hu>
2019-03-17 17:47:03 +0100

    #5: user model base created

---
+++ webcentral-backend-core/src/main/java/hu/bme/sch/bss/webcentral/model/User.java
@@ -0,0 +1,10 @@
+package hu.bme.sch.bss.webcentral.model;
+
+import hu.bme.sch.bss.webcentral.DomainAuditModel;
+
+import javax.persistence.Entity;
+
+@Entity
+public class User extends DomainAuditModel {
+
+}
