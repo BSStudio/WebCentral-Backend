f66e3b43da48408351c5a5f06970b18abc268ba3
tschik <csikbence@edu.bme.hu>
2019-03-26 14:06:55 +0100

    model.User tests created

--- webcentral-backend-core/src/main/java/hu/bme/sch/bss/webcentral/core/model/User.java
+++ webcentral-backend-core/src/main/java/hu/bme/sch/bss/webcentral/core/model/User.java
@@ -37,10 +37,16 @@ public final class User extends DomainAuditModel {
     private String imageURI;
 
     public User() {
+        // No-arg constructor for hibernate
     }
 
-    public static Builder builder() {
-        return new Builder();
+    public User(final Builder builder) {
+        this.nickname = builder.nickname;
+        this.givenName = builder.givenName;
+        this.familyName = builder.familyName;
+        this.email = builder.email;
+        this.description = builder.description;
+        this.imageURI = builder.imageURI;
     }
 
     public Long getId() {
@@ -71,14 +77,11 @@ public final class User extends DomainAuditModel {
         return imageURI;
     }
 
-    public User(final Builder builder) {
-        this.nickname = builder.nickname;
-        this.givenName = builder.givenName;
-        this.familyName = builder.familyName;
-        this.email = builder.email;
-        this.description = builder.description;
-        this.imageURI = builder.imageURI;
+    public static Builder builder() {
+        return new Builder();
     }
+
+    //Generated code begins here
 
     @Override
     public String toString() {
@@ -102,13 +105,13 @@ public final class User extends DomainAuditModel {
             return false;
         }
         User user = (User) o;
-        return Objects.equals(id, user.id) &&
-            Objects.equals(nickname, user.nickname) &&
-            Objects.equals(givenName, user.givenName) &&
-            Objects.equals(familyName, user.familyName) &&
-            Objects.equals(email, user.email) &&
-            Objects.equals(description, user.description) &&
-            Objects.equals(imageURI, user.imageURI);
+        return Objects.equals(id, user.id)
+            && Objects.equals(nickname, user.nickname)
+            && Objects.equals(givenName, user.givenName)
+            && Objects.equals(familyName, user.familyName)
+            && Objects.equals(email, user.email)
+            && Objects.equals(description, user.description)
+            && Objects.equals(imageURI, user.imageURI);
     }
 
     @Override
@@ -116,36 +119,38 @@ public final class User extends DomainAuditModel {
         return Objects.hash(id, nickname, givenName, familyName, email, description, imageURI);
     }
 
+    // Generated code ends here
+
     public static final class Builder {
 
-        private @NotBlank String nickname;
-        private @NotBlank String givenName;
-        private @NotBlank String familyName;
-        private @Email String email;
-        private @NotBlank String description;
+        private String nickname;
+        private String givenName;
+        private String familyName;
+        private String email;
+        private String description;
         private String imageURI;
 
-        public Builder withNickname(@NotBlank String nickname) {
+        public Builder withNickname(String nickname) {
             this.nickname = nickname;
             return this;
         }
 
-        public Builder withGivenName(@NotBlank String givenName) {
+        public Builder withGivenName(String givenName) {
             this.givenName = givenName;
             return this;
         }
 
-        public Builder withFamilyName(@NotBlank String familyName) {
+        public Builder withFamilyName(String familyName) {
             this.familyName = familyName;
             return this;
         }
 
-        public Builder withEmail(@Email String email) {
+        public Builder withEmail(String email) {
             this.email = email;
             return this;
         }
 
-        public Builder withDescription(@NotBlank String description) {
+        public Builder withDescription(String description) {
             this.description = description;
             return this;
         }
