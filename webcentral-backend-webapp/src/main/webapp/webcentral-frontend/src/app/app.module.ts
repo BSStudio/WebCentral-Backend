614ec5cb4f9c3afd044f97918958e95bf5df963c
Péter Veress <veress.peter@simonyi.bme.hu>
2019-04-15 17:43:40 +0200

    #113: ui navbar

--- webcentral-backend-webapp/src/main/webapp/webcentral-frontend/src/app/app.module.ts
+++ webcentral-backend-webapp/src/main/webapp/webcentral-frontend/src/app/app.module.ts
@@ -1,18 +1,31 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatIconModule } from '@angular/material/icon';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatSelectModule } from '@angular/material/select';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+   declarations: [
+      AppComponent,
+      NavbarComponent
+   ],
+   imports: [
+      BrowserModule,
+      AppRoutingModule,
+      BrowserAnimationsModule,
+      MatIconModule,
+      MatFormFieldModule,
+      MatSelectModule
+   ],
+   providers: [],
+   bootstrap: [
+      AppComponent
+   ]
 })
+
 export class AppModule { }
