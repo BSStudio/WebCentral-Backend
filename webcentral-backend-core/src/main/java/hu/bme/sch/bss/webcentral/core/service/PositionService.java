d4f48edd88626b5c7459e0c1fe936ae4b6fe8d4c
csikbence@edu.bme.hu <csikbence@edu.bme.hu>
2019-04-24 22:45:46 +0200

    #17: get position implemented

--- webcentral-backend-core/src/main/java/hu/bme/sch/bss/webcentral/core/service/PositionService.java
+++ webcentral-backend-core/src/main/java/hu/bme/sch/bss/webcentral/core/service/PositionService.java
@@ -6,12 +6,18 @@ import hu.bme.sch.bss.webcentral.core.model.Position;
 import org.slf4j.Logger;
 import org.springframework.stereotype.Component;
 
+import java.util.NoSuchElementException;
+import java.util.Optional;
+
 @Component
 @SuppressWarnings("designforextension")
 public class PositionService {
 
     private static final String USER_POST_CREATE_STARTED = "User creation started. {}";
     private static final String USER_POST_CREATE_SUCCEED = "User creation succeed. {}";
+    private static final String POSITION_SEARCH_STARTED = "User search started {}";
+    private static final String POSITION_SEARCH_SUCCEED = "User search succeed {}";
+    private static final String POSITION_NOT_FOUND = "User not found with id: {}";
 
     private final PositionDao positionDao;
     private  final Logger logger;
@@ -33,5 +39,16 @@ public class PositionService {
         return Position.builder()
             .withName(request.getName())
             .build();
+    }
+
+    public Position findById(final Long id) {
+        logger.info(POSITION_SEARCH_STARTED, id);
+        Optional<Position> result = positionDao.findById(id);
+        if (result.isEmpty()) {
+            logger.warn(POSITION_NOT_FOUND, id);
+            throw new NoSuchElementException("Position not found");
+        }
+        logger.info(POSITION_SEARCH_SUCCEED, id);
+        return result.get();
     }
 }
--- webcentral-backend-webapp/src/main/java/hu/bme/sch/bss/webcentral/controller/core/PositionController.java
+++ webcentral-backend-webapp/src/main/java/hu/bme/sch/bss/webcentral/controller/core/PositionController.java
@@ -2,7 +2,9 @@ package hu.bme.sch.bss.webcentral.controller.core;
 
 import hu.bme.sch.bss.webcentral.core.domain.PositionRequest;
 import hu.bme.sch.bss.webcentral.core.domain.PositionResponse;
+import hu.bme.sch.bss.webcentral.core.domain.UserResponse;
 import hu.bme.sch.bss.webcentral.core.model.Position;
+import hu.bme.sch.bss.webcentral.core.model.User;
 import hu.bme.sch.bss.webcentral.core.service.PositionService;
 
 import javax.validation.Valid;
@@ -10,6 +12,8 @@ import javax.validation.Valid;
 import org.slf4j.Logger;
 
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -24,6 +28,7 @@ public class PositionController {
 
 
     private static final String REQUEST_POSITION_CREATE = "Request for position creation received. {}";
+    private static final String REQUEST_POSITION_SEARCH = "Request for position search received with id of: {}";
     private final PositionService positionService;
     private final Logger logger;
 
@@ -37,6 +42,14 @@ public class PositionController {
     public final PositionResponse createUser(@Valid @RequestBody final PositionRequest request) {
         logger.info(REQUEST_POSITION_CREATE, request);
         Position result = positionService.create(request);
+        return new PositionResponse(result);
+    }
+
+    @GetMapping("/{id}")
+    @ResponseStatus(HttpStatus.FOUND)
+    public final PositionResponse getPosition(@PathVariable("id") final Long id) {
+        logger.info(REQUEST_POSITION_SEARCH, id);
+        Position result = positionService.findById(id);
         return new PositionResponse(result);
     }
 }
