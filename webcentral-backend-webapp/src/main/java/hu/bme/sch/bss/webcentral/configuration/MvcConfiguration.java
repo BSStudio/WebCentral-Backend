ed68aaabb0ed5b007da4dd40d5fafd41d32c66fc
tschik <csikbence@edu.bme.hu>
2019-05-12 01:37:08 +0200

    #23: authentication implementation in progress

--- webcentral-backend-webapp/src/main/java/hu/bme/sch/bss/webcentral/configuration/MvcConfiguration.java
+++ webcentral-backend-webapp/src/main/java/hu/bme/sch/bss/webcentral/configuration/MvcConfiguration.java
@@ -1,9 +1,11 @@
 package hu.bme.sch.bss.webcentral.configuration;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.password.LdapShaPasswordEncoder;
 
 @Configuration
 @EnableWebSecurity
@@ -11,7 +13,26 @@ public class MvcConfiguration extends WebSecurityConfigurerAdapter {
 
     @Override
     protected final void configure(final HttpSecurity http) throws Exception {
-        http.csrf().disable().authorizeRequests().antMatchers("/**").permitAll();
+        http
+            .authorizeRequests()
+                .anyRequest().fullyAuthenticated()
+            .and()
+            .formLogin();
     }
 
+    @Override
+    protected final void configure(final AuthenticationManagerBuilder auth) throws Exception {
+        auth
+            .ldapAuthentication()
+                .userDnPatterns("OU=Budavári Schönherz Stúdió,OU=Simonyi Károly Szakkollégium,DC=sch,DC=bme,DC=hu")
+                .groupSearchBase("OU=Users")
+                .contextSource()
+                    .url("")
+                    .managerDn("olvasok@sch.bme.hu")
+                    .managerPassword("")
+                    .and()
+                .passwordCompare()
+                    .passwordEncoder(new LdapShaPasswordEncoder())
+                    .passwordAttribute("userPassword");
+    }
 }
