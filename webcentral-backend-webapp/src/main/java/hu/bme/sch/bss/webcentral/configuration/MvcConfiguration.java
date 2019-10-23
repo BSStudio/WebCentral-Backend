d9a2b0871fc36b535d6b42cd0fb063774af66a9e
Csik Bence <22349790+csikb@users.noreply.github.com>
2019-10-23 23:45:46 +0200

    #23: test with in memory user

--- webcentral-backend-webapp/src/main/java/hu/bme/sch/bss/webcentral/configuration/MvcConfiguration.java
+++ webcentral-backend-webapp/src/main/java/hu/bme/sch/bss/webcentral/configuration/MvcConfiguration.java
@@ -1,9 +1,11 @@
 package hu.bme.sch.bss.webcentral.configuration;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
 
 @Configuration
 @EnableWebSecurity
@@ -11,7 +13,23 @@ public class MvcConfiguration extends WebSecurityConfigurerAdapter {
 
     @Override
     protected final void configure(final HttpSecurity http) throws Exception {
-        http.csrf().disable().authorizeRequests().antMatchers("/**").permitAll();
+        http.csrf().disable();
+        http
+                .authorizeRequests()
+                .anyRequest().fullyAuthenticated()
+                .and()
+                .formLogin();
     }
+
+    @Override
+    protected void configure(final AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication()
+                .withUser(User.withDefaultPasswordEncoder()
+                        .username("user")
+                        .password("password")
+                        .roles("USER")
+                        .build());
+    }
+
 
 }
