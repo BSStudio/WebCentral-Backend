de79aeffd0b20e272b5e0766ebd3869a02822faf
csikb <22349790+csikb@users.noreply.github.com>
2019-11-19 19:21:25 +0100

    #23: WIP

--- webcentral-backend-webapp/src/main/java/hu/bme/sch/bss/webcentral/configuration/MvcConfiguration.java
+++ webcentral-backend-webapp/src/main/java/hu/bme/sch/bss/webcentral/configuration/MvcConfiguration.java
@@ -6,10 +6,22 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.User;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @Configuration
 @EnableWebSecurity
 public class MvcConfiguration extends WebSecurityConfigurerAdapter {
+
+    private static final String PASSWORD_ATTRIBUTE = "password";
+    private static final Integer B_CRYPT_STRENGTH = 31;
+
+    //@Value("hu.bme.sch.bss.webcentral.userDnPatterns")
+    private String userDnPatterns = "OU=Users,OU=Budavári Schönherz Stúdió,OU=Simonyi Károly Szakkollégium,DC=sch,DC=bme,DC=hu";
+    //@Value("hu.bme.sch.bss.webcentral.userSearchBase")
+    private String userSearchBase = "CN=bss admin,OU=Users,OU=Budavári Schönherz Stúdió,OU=Simonyi Károly Szakkollégium,DC=sch,DC=bme,DC=hu";
+    //7@Value("hu.bme.sch.bss.webcentral.ldap_url")
+    private String ldap_url = "ldaps://dc.sch.bme.hu";
+
 
     @Override
     protected final void configure(final HttpSecurity http) throws Exception {
@@ -28,8 +40,44 @@ public class MvcConfiguration extends WebSecurityConfigurerAdapter {
                         .username("user")
                         .password("password")
                         .roles("USER")
-                        .build());
-    }
+                        .build()
+                );
+        auth.ldapAuthentication()
+                .userDnPatterns(userDnPatterns)
+                .userSearchBase(userSearchBase)
+                .contextSource()
+                .url(ldap_url)
+                .and()
+                .passwordCompare()
+                .passwordEncoder(new BCryptPasswordEncoder(B_CRYPT_STRENGTH))
+                .passwordAttribute(PASSWORD_ATTRIBUTE);
 
+        /* example code
+        auth.ldapAuthentication()
+                .authoritiesMapper(authorities -> {
+                    return authorities;
+                })
+                .contextSource()
+                    .url("")
+                    .managerDn("")
+                    .managerPassword("")
+                    .root("")
+                    .port(1)
+                    .and()
+                .groupRoleAttribute("")
+                .groupSearchBase("")
+                .groupSearchFilter("")
+                .ldapAuthoritiesPopulator(new DefaultLdapAuthoritiesPopulator(new DefaultSpringSecurityContextSource(""), ""))
+                .passwordCompare()
+                    .passwordEncoder(new BCryptPasswordEncoder())
+                    .passwordAttribute("")
+                    .and()
+                .rolePrefix("")
+                .userDetailsContextMapper(new LdapUserDetailsMapper())
+                .userDnPatterns()
+                .userSearchBase("")
+                .userSearchFilter("")
+                .withObjectPostProcessor(null);*/
+    }
 
 }
